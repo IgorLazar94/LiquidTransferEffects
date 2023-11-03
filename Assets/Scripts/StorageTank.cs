af4a97d67f94f8fab13ffe348fd9b6bd7d4a4c1e
IgorLazar <81099964+IgorLazar94@users.noreply.github.com>
2023-11-03 20:07:54 +0200

    modified storage tank, add open\close drain system + UI buttons

--- Assets/Scripts/StorageTank.cs
+++ Assets/Scripts/StorageTank.cs
@@ -1,8 +1,58 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class StorageTank : MonoBehaviour
 {
+    public bool DrainIsOpen { get; private set; }
     private int waterCounter;
+    [SerializeField] private Transform leftDrain;
+    [SerializeField] private Transform rightDrain;
+    private float timeToOpenDrain = 0.5f;
+    private float openDrainPos = 5f;
+    private float closeDrainPos = 1.5f;
+
+    private void Start()
+    {
+        DrainIsOpen = false;
+    }
+
+    private void OpenDrain()
+    {
+        rightDrain.DOLocalMoveX(openDrainPos, timeToOpenDrain);
+        leftDrain.DOLocalMoveX(-openDrainPos, timeToOpenDrain);
+    }
+
+    private void CloseDrain()
+    {
+        rightDrain.DOLocalMoveX(closeDrainPos, timeToOpenDrain);
+        leftDrain.DOLocalMoveX(-closeDrainPos, timeToOpenDrain);
+    }
+
+    public void SwitchDrainState()
+    {
+        if (!DrainIsOpen)
+        {
+            OpenDrain();
+            DrainIsOpen = true;
+        }
+        else
+        {
+            CloseDrain();
+            DrainIsOpen = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            OpenDrain();
+        }
+        else if (Input.GetKeyDown(KeyCode.C))
+        {
+            CloseDrain();
+        }
+    }
 }
--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -5,8 +5,11 @@ using UnityEngine.UI;
 
 public class UIController : MonoBehaviour
 {
-    [SerializeField] private Pipe pipe;
     [SerializeField] private Image closeValveButton, openValveButton;
+    [SerializeField] private Image drainLeftButton, drainRightButton;
+    [SerializeField] private Pipe pipe;
+    [SerializeField] private StorageTank storageTankLeft;
+    [SerializeField] private StorageTank storageTankRight;
 
     private void Start()
     {
@@ -30,6 +33,33 @@ public class UIController : MonoBehaviour
         {
             openValveButton.color = Color.white;
             closeValveButton.color = Color.green;
+        }
+    }
+
+    public void OpenLeftDrain()
+    {
+        Debug.Log(storageTankLeft.DrainIsOpen + " is open");
+        storageTankLeft.SwitchDrainState();
+        if (!storageTankLeft.DrainIsOpen)
+        {
+            drainLeftButton.color = Color.white;
+        }
+        else
+        {
+            drainLeftButton.color = Color.red;
+        }
+    }
+
+    public void OpenRightDrain()
+    {
+        storageTankRight.SwitchDrainState();
+        if (!storageTankRight.DrainIsOpen)
+        {
+            drainRightButton.color = Color.white;
+        }
+        else
+        {
+            drainRightButton.color = Color.red;
         }
     }
 }
