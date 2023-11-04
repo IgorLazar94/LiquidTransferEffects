aaa42877484a3ab662945d1525c4b98a8034e91b
IgorLazar <81099964+IgorLazar94@users.noreply.github.com>
2023-11-04 15:00:09 +0200

    clear unnecessary code

--- Assets/Scripts/ObjectPool/PoolController.cs
+++ Assets/Scripts/ObjectPool/PoolController.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PoolController : MonoBehaviour
@@ -12,24 +10,13 @@ public class PoolController : MonoBehaviour
 
     private void Start()
     {
-        //GetSettingsValues();
         this.pool = new LiquidPool<LiquidElement>(liquidElementPrefab, poolCount, this.transform);
         this.pool.autoExpand = this.autoExpand;
     }
-    //private void GetSettingsValues()
-    //{
-    //    poolCount = GameSettings.Instance.GetPoolCount();
-    //    autoExpand = GameSettings.Instance.GetPoolAutoExpand();
-    //    playerBulletPrefab = GameSettings.Instance.GetPlayerBulletPrefab();
-    //}
 
     public LiquidElement CreateLiquidElement()
     {
         var bullet = this.pool.GetFreeElement();
         return bullet;
     }
-
-
-
-
 }
